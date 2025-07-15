75c737e1e0adff469764ddc0b53a8e09f4d3f54c
Derek Wu <derekwu@macbookpro.mynetworksettings.com>
2025-07-14 21:33:58 -0400

    remove backend package

--- src/solvers/__init__.py
+++
@@ -1,6 +0,0 @@
-from .backward_induction import BackwardInductionSolver
-from .mixed_strategy import MixedStrategySolver
-from .pure_strategy import PureStrategyNashSolver
-from .solver import Solver
-
-__all__ = ['Solver','PureStrategyNashSolver', 'MixedStrategySolver', 'BackwardInductionSolver' ]
